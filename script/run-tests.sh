8ff4d30b962716e750acc6cf55022c76ab214d43
hrithik-k0 <145096843+hrithik-k0@users.noreply.github.com>
2025-10-04 16:03:48 +0530

    Update run-tests.sh

--- script/run-tests.sh
+++ script/run-tests.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 
-docker run -d \
-  --name jenkins \
-  -p 8080:8080 \
-  -p 50000:50000 \
-  -v jenkins_home:/var/jenkins_home \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  jenkins/jenkins:lts
-
 echo "Running health check..."
 
 #wait for app to start (if needed)
